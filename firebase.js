6ad85f016d8ad79d6f4d2378e8a91626640fa198
monetaryearn-commits <monetaryearn@gmail.com>
2025-11-04 11:32:51 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,21 +3,23 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import {
   getDatabase,
   ref as dbRef,
-  get,
   runTransaction,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
+// 👇 Paste your config here from Firebase Console
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_FIREBASE_PROJECT.firebaseio.com",
-  projectId: "YOUR_FIREBASE_PROJECT",
-  storageBucket: "YOUR_FIREBASE_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC57SuWwG_zYznXtJLsvN9EbPMkJUPO3yw",
+  authDomain: "tnvotecount-54848.firebaseapp.com",
+  databaseURL: "https://tnvotecount-54848-default-rtdb.firebaseio.com",
+  projectId: "tnvotecount-54848",
+  storageBucket: "tnvotecount-54848.firebasestorage.app",
+  messagingSenderId: "314648286297",
+  appId: "1:314648286297:web:712a67a5df6a5f39a0cd48"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, dbRef, get, runTransaction };
+export { db, dbRef, runTransaction };
