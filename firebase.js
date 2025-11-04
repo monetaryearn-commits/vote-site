f8c11ccf714714489c507b7c6cf6aefc0342dbe6
monetaryearn-commits <monetaryearn@gmail.com>
2025-11-04 11:11:54 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,23 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.14.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getDatabase,
-  ref,
+  ref as dbRef,
   get,
-  update,
-  runTransaction
-} from "https://www.gstatic.com/firebasejs/11.14.0/firebase-database.js";
+  runTransaction,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET.appspot.com",
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_FIREBASE_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_FIREBASE_PROJECT.firebaseio.com",
+  projectId: "YOUR_FIREBASE_PROJECT",
+  storageBucket: "YOUR_FIREBASE_PROJECT.appspot.com",
   messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export const dbRef = ref;
-export { get, update, runTransaction };
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, dbRef, get, runTransaction };
